54c811900a0021865ab39e8113ec792d26293456
Kirill Boldyrev <bke@skbkontur.ru>
2021-05-14 11:56:12 +0500

    Implement UpcExtensions.CalculateUpc()

--- cs/ControlDigit/Upc/UpcExtensions.cs
+++ cs/ControlDigit/Upc/UpcExtensions.cs
@@ -6,7 +6,34 @@ namespace ControlDigit
     {
         public static int CalculateUpc(this long number)
         {
-            throw new NotImplementedException();
+            var n = number.ToString().Length;
+            long result = 0;
+
+            var parsedNumber = number;
+
+            for (var i = 1; i <= n; i++)
+            {
+                var position = n - (n - i + 1) + 1;
+                var numberOnPosition = parsedNumber % 10;
+
+                if (position % 2 == 1)
+                {
+                    result = result + numberOnPosition * 3;
+                }
+                else
+                {
+                    result = result + numberOnPosition;
+                }
+
+                parsedNumber = parsedNumber / 10;
+            }
+
+            result = result % 10;
+
+            if (result != 0)
+                result = 10 - result;
+
+            return (int) result;
         }
     }
 }
