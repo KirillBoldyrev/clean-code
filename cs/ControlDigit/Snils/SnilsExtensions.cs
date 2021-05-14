0e53fb033f5806b020d70c374d4207f0a31d77ae
Kirill Boldyrev <bke@skbkontur.ru>
2021-05-14 13:07:30 +0500

    Implement SnilsExtensions.CalculateSnils()

--- cs/ControlDigit/Snils/SnilsExtensions.cs
+++ cs/ControlDigit/Snils/SnilsExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ControlDigit
 {
@@ -6,7 +8,51 @@ namespace ControlDigit
     {
         public static int CalculateSnils(this long number)
         {
-            throw new NotImplementedException();
+            int result = 0;
+
+            var digitsByPosition = GetDigitsByPositionFromEnd(number);
+
+            result = digitsByPosition
+                .Select(kv => kv.Key * kv.Value)
+                .Sum();
+
+            result = ProcessBelow101Cases(result);
+
+            if (result > 101)
+            {
+                result = ProcessBelow101Cases(result % 101);
+            }
+
+            return result;
         }
+
+        private static Dictionary<int, int> GetDigitsByPositionFromEnd(long number)
+        {
+            var n = number.ToString().Length;
+            var result = new Dictionary<int, int>();
+
+            var parsedNumber = number;
+
+            for (var positionFromEnd = 1; positionFromEnd <= n; positionFromEnd++)
+            {
+                var numberOnPosition = parsedNumber % 10;
+                result.Add(positionFromEnd, (int) numberOnPosition);
+                parsedNumber = parsedNumber / 10;
+            }
+
+            return result;
+        }
+
+        private static int ProcessBelow101Cases(int number)
+        {
+            if (number < 100)
+                return number;
+
+            if (number == 100 || number == 101)
+                return 0;
+
+            return number;
+        }
+
     }
 }
